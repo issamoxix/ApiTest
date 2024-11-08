189292da42ffea52975ff6f675580c3ad2f6c818
Issam Haidaoui <issamhaidaoui@gmail.com>
2024-11-08 19:50:17 +0100

    websocket send names

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from time import sleep
 
 app = FastAPI()
 
-
+names = ["John", "Jane", "Bob", "Alice"]
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     try:
-        while True:
-            data = await websocket.receive_text()
-            if data.lower() == "exit":
-                await websocket.send_text("Goodbye!")
-                break
-            await websocket.send_json({"message": data})
+        for name in names:
+            await websocket.send_text(name)
+            sleep(1)
     except WebSocketDisconnect:
         print("Client disconnected")
     finally:
